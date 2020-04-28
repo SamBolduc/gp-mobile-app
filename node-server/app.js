c6d2b471401b4b85d9e33a4512175cbf079d187c
Raphaël Thériault <raphael_theriault@outlook.com>
2020-04-28 17:16:27 -0400

    Enlever les trucs inutiles

--- node-server/app.js
+++ node-server/app.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const path = require("path");
 const router = require("./routes");
+
 require("./database");
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "public")));
 app.use("/", router);
 
 app.listen(3000, "192.168.2.17");
--- node-server/routes/index.js
+++ node-server/routes/index.js
@@ -1,10 +1,6 @@
 const router = require("express").Router();
 const boxesRoute = require("./boxes.routes");
 
-router.get("/", (req, res) => {
-  res.render("home");
-});
-
 router.use("/boxes", boxesRoute);
 
 module.exports = router;
