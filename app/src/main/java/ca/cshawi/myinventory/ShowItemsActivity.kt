697a69c96e6b6083fb69ce94a6ad20eeac66bf47
SamB <sambolduc4@gmail.com>
2020-04-29 01:04:50 -0400

    Added options on the floating button

--- app/src/main/java/ca/cshawi/myinventory/ShowItemsActivity.kt
+++ app/src/main/java/ca/cshawi/myinventory/ShowItemsActivity.kt
@@ -3,13 +3,14 @@ package ca.cshawi.myinventory
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.NavigationView
-import android.support.design.widget.Snackbar
+import android.support.v4.content.res.ResourcesCompat
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.helper.ItemTouchHelper
+import android.util.Log
 import android.view.MenuItem
 import ca.cshawi.myinventory.api.APIService
 import ca.cshawi.myinventory.api.ActionResponse
@@ -17,6 +18,8 @@ import ca.cshawi.myinventory.api.requests.UpdateItemsRequest
 import ca.cshawi.myinventory.boxes.Box
 import ca.cshawi.myinventory.boxes.items.ItemAdapter
 import ca.cshawi.myinventory.boxes.items.ItemSwipeController
+import com.leinardi.android.speeddial.SpeedDialActionItem
+import com.leinardi.android.speeddial.SpeedDialView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import retrofit2.Call
@@ -30,42 +33,89 @@ class ShowItemsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSe
     var boxPosition = -1
     lateinit var box: Box
 
+    private val speedDial: SpeedDialView by lazy { findViewById<SpeedDialView>(R.id.speedDial) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_show_items)
 
-        fab.setImageResource(R.drawable.round_save_white_36)
-        fab.setOnClickListener { view ->
 
-            box.items.forEach {
-                it.currentAmount += it.changedQuantity
-                it.changedQuantity = 0
-                adapter.notifyDataSetChanged()
-            }
-
-            APIService.INSTANCE.updateItems(UpdateItemsRequest(box.id, box.items).toString())
-                .enqueue(object :
-                    Callback<ActionResponse> {
-                    override fun onFailure(call: Call<ActionResponse>, t: Throwable) {
-                        t.printStackTrace()
-                    }
-
-                    override fun onResponse(
-                        call: Call<ActionResponse>,
-                        response: Response<ActionResponse>
-                    ) {
-
-                        box.modif = 0
-                        Snackbar.make(
-                            view,
-                            "Les modifications ont été sauvegardé avec succès",
-                            Snackbar.LENGTH_LONG
-                        ).setAction("Quitter") { onBackPressed() }.show()
-                    }
-                });
+        speedDial.expansionMode = SpeedDialView.ExpansionMode.TOP
+        speedDial.setOnClickListener {
+            Log.i("Here", "Salut")
         }
 
+
+        speedDial.addActionItem(
+            SpeedDialActionItem.Builder(
+                R.id.fab_new, R.drawable
+                    .baseline_add_white_36
+            ).setFabBackgroundColor(
+                ResourcesCompat.getColor(
+                    resources,
+                    R.color.red,
+                    theme
+                )
+            ).setLabel("Ajouter un objet")
+                .create()
+        )
+
+        speedDial.addActionItem(
+            SpeedDialActionItem.Builder(
+                R.id.fab_save, R.drawable
+                    .round_save_white_36
+            ).setFabBackgroundColor(
+                ResourcesCompat.getColor(
+                    resources,
+                    R.color.green,
+                    theme
+                )
+            ).setLabel("Enregistrer et Quitter")
+                .create()
+        )
+
+        speedDial.setOnActionSelectedListener(SpeedDialView.OnActionSelectedListener { actionItem ->
+            when (actionItem.id) {
+                R.id.fab_new -> {
+                    //TODO: Add new item
+                    speedDial.close()
+                    return@OnActionSelectedListener true
+                }
+                R.id.fab_save -> {
+                    box.items.forEach {
+                        it.currentAmount += it.changedQuantity
+                        it.changedQuantity = 0
+                        adapter.notifyDataSetChanged()
+                    }
+
+                    APIService.INSTANCE.updateItems(
+                        UpdateItemsRequest(
+                            box.id,
+                            box.items
+                        ).toString()
+                    )
+                        .enqueue(object :
+                            Callback<ActionResponse> {
+                            override fun onFailure(call: Call<ActionResponse>, t: Throwable) {
+                                t.printStackTrace()
+                            }
+
+                            override fun onResponse(
+                                call: Call<ActionResponse>,
+                                response: Response<ActionResponse>
+                            ) {
+
+                                box.modif = 0
+                                onBackPressed()
+                                speedDial.close()
+                            }
+                        });
+                    return@OnActionSelectedListener true
+                }
+            }
+            true
+        })
 
         setSupportActionBar(toolbar)
         val toggle = ActionBarDrawerToggle(
