424887b36f569de131eb38d9533e4938c2c50f48
JY-Denoncourt <sambolduc4@gmail.com>
2020-04-29 10:15:58 -0400

    Missing files

--- node-server/app.js
+++ node-server/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const router = require("./routes");
+const path = require("path");
 const logger = require("./middlewares/logger");
 
 require("./database");
@@ -12,6 +13,9 @@ app.use(logger);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", router);
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "pug");
 
 const url = process.env.SRV_URL;
 const port = Number(process.env.SRV_PORT);
